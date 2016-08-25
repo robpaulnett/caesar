fc3bc81964fb1de89bf18159d382d05ad3e5fc89
Paul <paulrobnett@hotmail.com>
2016-08-24 22:54:19 -0500

    formation part 1 99% there

--- main.py
+++ main.py
@@ -16,6 +16,7 @@
 #
 import webapp2
 from caesar import encrypt
+import cgi
 
 # html boilerplate for the top of every page
 page_header = """
@@ -26,7 +27,7 @@ page_header = """
     <style type="text/css">
         .error {
             color: red;
-        }
+            }
     </style>
 </head>
 <body>
@@ -34,46 +35,54 @@ page_header = """
         <a href="/">Caesar Encryption</a>
     </h3>
 """
-
 # html boilerplate for the bottom of every page
 page_footer = """
 </body>
 </html>
 """
 
-class MainHandler(webapp2.RequestHandler):
+# a form for creating an encryption (rot13 or rot*)
+rotate_form_input = """
+<form action="/" method="POST">
+    <label>
+        <strong>Rotate by:</strong>
+        <input type="text" name="rotatetimes" value="%(rotatetimes)s"/>
+    </label>
+    <br /><br />
+    <label>
+        <strong>Encrpytion:</strong>
+        <br />
+        <input type="text" name="encrypttext" size="250" style="height:10em;width:35em;" value="%(encrypttext)s"/>
+    </label>
+    <br />
+    <input type="submit" value="Submit Query"/>
+</form>
+"""
+
+class Index(webapp2.RequestHandler):
     """ Handles requests coming in to '/' (the root of our site)
         e.g. www.caesar.com/
     """
+    def write_form(self, rotatetimes="", encrypttext=""):
+        self.response.out.write(rotate_form_input % {"rotatetimes": rotatetimes,
+                                                        "encrypttext": encrypttext})
+
     def get(self):
-    #    self.response.write('Hello world!')
+        #PUT FORM ON SCREEN
+        #response = page_header + rotate_form_input + page_footer
+        self.write_form()
 
-        #crypto_header = "<h4>Do An Encryption</h4>"
-
-        # a form for creating an encryption (rot13 or rot*)
-        encrypt_form_input = """
-        <form action="/rotall" method="GET">
-            <label>
-                <strong>Rotate by:</strong>
-                <input type="text" name="encrypt_start"/>
-            </label>
-            <!--  #<div>%(error)s</div>    -->
-            <br /><br />
-            <label>
-                <strong>Encrpyted Results:</strong>
-                <br />
-                <textarea type="text" name="encrypt_end" size="250" style="height:10em;width:35em;" value="self.response.write(encrypt_form_input)"></textarea>
-            </label>
-            <br />
-            <input type="submit" value="Submit Query"/>
-        </form>
-        """
+    def post(self):
+        # look inside the request to figure out what the user typed
+        number_rotations = self.request.get('rotatetimes')
+        user_raw_inputs = self.request.get('encrypttext')
+        encrypted_item = encrypt(user_raw_inputs, number_rotations)
         # combine all the pieces to build the content of our response
-        main_content = encrypt_form_input
+        main_content = encrypted_item
         response = page_header + main_content + page_footer
-        self.response.write(response)
+        self.response.out.write(response)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/rotall', MainHandler)
+('/', Index),
 ], debug=True)
